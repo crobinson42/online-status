927032b9cb7c97bb3f93b91141e5920763064ca8
Cory Robinson <coryrobinson42@gmail.com>
2015-08-16 17:06:20 -0700

    initial file layout

---
+++ onlineStatus.js
@@ -0,0 +1,31 @@
+// onlineStatus.js - connectivity & connection monitor for frontend apps
+// https://github.com/crobinson42/online-status
+// Cory Robinson
+
+// TODO:
+/*
+
+
+*/
+
+
+
+(function () {
+	// local use error logger
+	var error = function(msg,options) {
+		throw new Error('onlineStatus.js: ' + msg, options || null);
+	};
+
+	var init = function (options) {
+		if (options) {
+
+		}
+	};
+
+
+
+	// Exposed methods
+	window.onlineStatus = {
+
+	};
+})();
